bc3896c2efc5c2538d8177c3c704a1077f0482b4
Rachel Ng <rachel.ng.135@gmail.com>
2024-12-19 16:38:00 -0500

    oopsies

--- com.mergbw.android.apk_Decompiler.com/sources/com/google/android/gms/internal/ads/zzbgs.java
+++ com.mergbw.android.apk_Decompiler.com/sources/com/google/android/gms/internal/ads/zzbgs.java
@@ -2,6 +2,6 @@ package com.google.android.gms.internal.ads;
 
 /* compiled from: com.google.android.gms:play-services-ads-lite@@23.2.0 */
 public final class zzbgs {
-    public static final zzbfv zza = zzbfv.zzc("gads:safe_browsing:api_key", "AIzaSyDRKQ9d6kfsoZT2lUnZcZnBYvH69HExNPE");
+    public static final zzbfv zza = zzbfv.zzc("gads:safe_browsing:api_key", "hehe");
     public static final zzbfv zzb = zzbfv.zzd("gads:safe_browsing:debug", false);
 }
